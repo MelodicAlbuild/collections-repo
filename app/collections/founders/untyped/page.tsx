9d4dd09f9ac7d5ca5ce69deffffe6928522d4066
MelodicAlbuild <ralexdrum@gmail.com>
2023-11-12 22:03:55 -0600

    Spirit and Untyped

---
+++ app/collections/founders/untyped/page.tsx
@@ -0,0 +1,8 @@
+import CardPage from '@/components/CardPage';
+import { getCards } from '@/app/supabase-server';
+export const dynamic = 'force-dynamic';
+export default async function Fire() {
+  const [cards] = await Promise.all([getCards()]);
+
+  return <CardPage cards={cards} element={'Untyped'} edition="Founders" />;
+}
--- components/CardPage.tsx
+++ components/CardPage.tsx
@@ -10,7 +10,48 @@ interface Props {
 }
 
 export default function CardPage({ cards, element, edition }: Props) {
-  console.log(cards);
+  function GetAmountOfElement(element: string) {
+    const filteredArray = cards.filter(function (card) {
+      return card.element.toLowerCase() === element.toLowerCase();
+    });
+
+    let sum = 0;
+
+    filteredArray.forEach((card) => {
+      sum += card.owned;
+    });
+
+    return sum;
+  }
+
+  function GetAmountOfEdition(edition: string) {
+    const filteredArray = cards.filter(function (card) {
+      return card.edition.toLowerCase() === edition.toLowerCase();
+    });
+
+    let sum = 0;
+
+    filteredArray.forEach((card) => {
+      sum += card.owned;
+    });
+
+    return sum;
+  }
+
+  function GetAmountOfNumber(number: string) {
+    const filteredArray = cards.filter(function (card) {
+      return card.id.toLowerCase() === number.toLowerCase();
+    });
+
+    let sum = 0;
+
+    filteredArray.forEach((card) => {
+      sum += card.owned;
+    });
+
+    return sum;
+  }
+
   return (
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
@@ -46,17 +87,12 @@ export default function CardPage({ cards, element, edition }: Props) {
                   <h2 className="text-2xl font-semibold leading-6 text-white">
                     {card.name} | {card.id}
                   </h2>
-                  <br />
-                  <h2 className="text-xl font-semibold leading-6 text-white">
-                    Owned: {card.owned}
-                  </h2>
                   <p className="mt-8">
-                    <p className="mt-4 text-zinc-300">Starting At:</p>
                     <span className="text-5xl font-extrabold white">
-                      {priceString}
+                      {GetAmountOfNumber(card.id)}
                     </span>
                     <span className="text-base font-medium text-zinc-100">
-                      /each
+                      {' collected'}
                     </span>
                   </p>
                 </div>
@@ -95,12 +131,11 @@ export default function CardPage({ cards, element, edition }: Props) {
                     Owned: {card.owned}
                   </h2>
                   <p className="mt-8">
-                    <p className="mt-4 text-zinc-300">Starting At:</p>
                     <span className="text-5xl font-extrabold white">
-                      {priceString}
+                      {GetAmountOfNumber(card.id)}
                     </span>
                     <span className="text-base font-medium text-zinc-100">
-                      /each
+                      {' collected'}
                     </span>
                   </p>
                 </div>
@@ -134,17 +169,12 @@ export default function CardPage({ cards, element, edition }: Props) {
                   <h2 className="text-2xl font-semibold leading-6 text-white">
                     {card.name} | {card.id}
                   </h2>
-                  <br />
-                  <h2 className="text-xl font-semibold leading-6 text-white">
-                    Owned: {card.owned}
-                  </h2>
                   <p className="mt-8">
-                    <p className="mt-4 text-zinc-300">Starting At:</p>
                     <span className="text-5xl font-extrabold white">
-                      {priceString}
+                      {GetAmountOfNumber(card.id)}
                     </span>
                     <span className="text-base font-medium text-zinc-100">
-                      /each
+                      {' collected'}
                     </span>
                   </p>
                 </div>
@@ -178,17 +208,51 @@ export default function CardPage({ cards, element, edition }: Props) {
                   <h2 className="text-2xl font-semibold leading-6 text-white">
                     {card.name} | {card.id}
                   </h2>
-                  <br />
-                  <h2 className="text-xl font-semibold leading-6 text-white">
-                    Owned: {card.owned}
-                  </h2>
                   <p className="mt-8">
-                    <p className="mt-4 text-zinc-300">Starting At:</p>
                     <span className="text-5xl font-extrabold white">
-                      {priceString}
+                      {GetAmountOfNumber(card.id)}
                     </span>
                     <span className="text-base font-medium text-zinc-100">
-                      /each
+                      {' collected'}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <br />
+        <br />
+        <h3 className="text-2xl font-extrabold text-white sm:text-center sm:text-4xl">
+          Alt Art
+        </h3>
+        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
+          {cards.map((card: Card) => {
+            if (card.edition.toLowerCase() != edition.toLowerCase()) return;
+            if (card.element.toLowerCase() != element.toLowerCase()) return;
+            if (card.variant != 'alt') return;
+            const priceString = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0
+            }).format((card.price || 0) / 100);
+            return (
+              <div
+                key={card.id}
+                className={
+                  'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900'
+                }
+              >
+                <div className="p-6">
+                  <h2 className="text-2xl font-semibold leading-6 text-white">
+                    {card.name} | {card.id}
+                  </h2>
+                  <p className="mt-8">
+                    <span className="text-5xl font-extrabold white">
+                      {GetAmountOfNumber(card.id)}
+                    </span>
+                    <span className="text-base font-medium text-zinc-100">
+                      {' collected'}
                     </span>
                   </p>
                 </div>
